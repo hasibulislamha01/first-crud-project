cbcf604b7873dcac9a3c4adc3cc1dfd5096b0ce9
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-04-03 22:44:55 +0600

    second commit

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return (
-        <div className="p-32 text-center bg-rose-200 text-5xl font-semibold">
+        <div className="p-32 text-center bg-rose-100 text-5xl font-semibold">
             Home Sweet Home
         </div>
     );
