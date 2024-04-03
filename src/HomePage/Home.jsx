4fa4e92404ea9d945266b4d4910bcdc5a363735b
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-04-03 22:41:58 +0600

    first commit

---
+++ src/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="p-32 text-center bg-rose-200 text-5xl font-semibold">
+            Home Sweet Home
+        </div>
+    );
+};
+
+export default Home;
